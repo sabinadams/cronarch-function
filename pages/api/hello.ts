12a288ec6e922ac88149d7a59517fd2110f0b6cb
Sabin Adams <sabintheworld@gmail.com>
2023-05-02 15:22:57 -0700

    Init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client/edge";
+import useAccelerate from "@prisma/extension-accelerate";
 
-type Data = {
-  name: string
-}
+const prisma = new PrismaClient().$extends(useAccelerate);
 
-export default function handler(
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const users = await prisma.user.findMany({
+    where: { id: 2 },
+    cacheStrategy: { swr: 60, ttl: 60 },
+  });
+
+  const data = JSON.stringify(users, null, 2);
+
+  return new Response(data, {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
 }
